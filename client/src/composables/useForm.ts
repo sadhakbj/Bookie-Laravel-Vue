031ecbaf80d162c56a77aaccd160c7f726ba59eb
Bijaya Prasad Kuikel <sadhakbj@gmail.com>
2024-01-12 13:09:06 +0900

    Some fixes and FE CI CD

--- client/src/composables/useForm.ts
+++ client/src/composables/useForm.ts
@@ -1,18 +1,17 @@
-import { isAxiosError } from "axios";
-import { reactive } from "vue";
-
+import { isAxiosError } from 'axios'
+import { reactive } from 'vue'
 
 interface FormErrors {
-  [key: string]: string[];
+  [key: string]: string[]
 }
 
 interface FormMethods<T> {
-  fields: T;
-  processing: boolean;
-  errors: FormErrors;
-  submit(submitter: (fields: T) => Promise<void>): Promise<void>;
-  hasError(field: string): boolean;
-  getError(field: string): string;
+  fields: T
+  processing: boolean
+  errors: FormErrors
+  submit(submitter: (fields: T) => Promise<void>): Promise<void>
+  hasError(field: string): boolean
+  getError(field: string): string
 }
 
 export default function useForm<T>(initialFields: any): FormMethods<T> {
@@ -21,28 +20,28 @@ export default function useForm<T>(initialFields: any): FormMethods<T> {
     processing: false,
     errors: {} as FormErrors,
     async submit(submitter: (fields: T) => Promise<void>): Promise<void> {
-      if (state.processing) return;
+      if (state.processing) return
 
-      state.errors = {};
-      state.processing = true;
+      state.errors = {}
+      state.processing = true
 
       try {
-        await submitter(state.fields);
+        await submitter(state.fields)
       } catch (error) {
         if (isAxiosError(error) && error.response?.status === 422) {
-          state.errors = error.response.data.errors;
+          state.errors = error.response.data.errors
         }
       }
 
-      state.processing = false;
+      state.processing = false
     },
     hasError(field: string): boolean {
-      return Object.prototype.hasOwnProperty.call(state.errors, field);
+      return Object.prototype.hasOwnProperty.call(state.errors, field)
     },
     getError(field: string): string {
-      return (state.errors[field] && state.errors[field][0]) || "";
-    },
-  });
+      return (state.errors[field] && state.errors[field][0]) || ''
+    }
+  })
 
-  return state;
+  return state
 }
--- client/src/views/auth/RegisterView.vue
+++ client/src/views/auth/RegisterView.vue
@@ -1,20 +1,34 @@
 <script lang="ts" setup>
+import useForm from '@/composables/useForm'
 import axios from '@/lib/axios'
-import { reactive } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
 
-const form = reactive({
+const router = useRouter()
+const authStore = useAuthStore()
+
+interface IRegisterCredentials {
+  name: string
+  email: string
+  password: string
+  password_confirmation: string
+}
+
+const form = useForm<IRegisterCredentials>({
   name: '',
   email: '',
   password: '',
   password_confirmation: ''
 })
+
 const handleRegister = async () => {
-  try {
+  await form.submit(async (fields) => {
     await axios.get('/sanctum/csrf-cookie')
-    await axios.post('/register', form)
-  } catch (error) {
-    alert('we have error')
-  }
+    const { data } = await axios.post('/register', fields)
+    const user = data.data
+    authStore.setCurrentUser(user)
+    await router.push('/')
+  })
 }
 </script>
 <template>
@@ -32,13 +46,18 @@ const handleRegister = async () => {
             <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
             <div class="mt-1">
               <input
-                v-model="form.name"
+                v-model="form.fields.name"
                 type="text"
                 name="name"
                 id="name"
                 class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                :class="{ 'border-red-500': form.hasError('password') }"
+                :disabled="form.processing"
               />
             </div>
+            <p class="text-xs mt-1 text-red-500" v-if="form.hasError('name')">
+              {{ form.getError('name') }}
+            </p>
           </div>
 
           <div class="mt-6">
@@ -47,26 +66,36 @@ const handleRegister = async () => {
             </label>
             <div class="mt-1">
               <input
-                v-model="form.email"
+                v-model="form.fields.email"
                 type="email"
                 name="email"
                 id="email"
                 class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                :class="{ 'border-red-500': form.hasError('password') }"
+                :disabled="form.processing"
               />
             </div>
+            <p class="text-xs mt-1 text-red-500" v-if="form.hasError('email')">
+              {{ form.getError('email') }}
+            </p>
           </div>
 
           <div class="mt-6">
             <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
             <div class="mt-1">
               <input
-                v-model="form.password"
+                v-model="form.fields.password"
                 type="password"
                 name="password"
                 id="password"
                 class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                :class="{ 'border-red-500': form.hasError('password') }"
+                :disabled="form.processing"
               />
             </div>
+            <p class="text-xs mt-1 text-red-500" v-if="form.hasError('password')">
+              {{ form.getError('password') }}
+            </p>
           </div>
 
           <div class="mt-6">
@@ -75,21 +104,33 @@ const handleRegister = async () => {
             </label>
             <div class="mt-1">
               <input
-                v-model="form.password_confirmation"
+                v-model="form.fields.password_confirmation"
                 type="password"
                 name="password_confirmation"
                 id="password_confirmation"
                 class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                :class="{ 'border-red-500': form.hasError('password_confirmation') }"
+                :disabled="form.processing"
               />
             </div>
+            <p class="text-xs mt-1 text-red-500" v-if="form.hasError('password_confirmation')">
+              {{ form.getError('password_confirmation') }}
+            </p>
           </div>
 
           <div class="mt-6">
             <button
               type="submit"
-              class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              :class="{
+                'bg-blue-400': form.processing,
+                'bg-blue-600': !form.processing,
+                'hover:bg-blue-600': !form.processing
+              }"
+              :disabled="form.processing"
+              :style="{ cursor: form.processing ? 'wait' : 'pointer' }"
             >
-              Register
+              {{ form.processing ? 'Loading...' : 'Register' }}
             </button>
           </div>
 
